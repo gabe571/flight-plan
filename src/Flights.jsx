e28001aecdfa8a0bb4902061f4964546d4a5537a
Gabe571 <gabriel.castro1028@gmail.com>
2021-02-28 17:44:43 -0800

    flight data mapped

--- src/Flights.jsx
+++ src/Flights.jsx
@@ -17,7 +17,19 @@ const Flights = () => {
     console.log(flights)
     return (
         <div>
-            
+            <h1> Flights </h1>
+            <ul className="flights">
+                {
+                    flights.map(flight => (
+                        <ul key={flight.id}>
+                           <h1>{flight.airline.name} {flight.airline.iata}</h1>
+                            <h3> Departing {flight.departure.airport} </h3>
+                            <h5> Gate {flight.departure.gate} </h5>
+                            <h3> Arrival {flight.arrival.airport}</h3>
+                             </ul>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
